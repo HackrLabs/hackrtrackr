7149aa28b55256a3538efd40840fb381e6adea8a
Danny Grove <danny@drgrovellc.com>
2013-11-02 18:31:12 -0400

    All Areas with Items and individual areas with items now work :)

--- libs/areas.js
+++ libs/areas.js
@@ -6,7 +6,7 @@ var pgConn = "postgres://" + config.postgres.user + ":" + config.postgres.passwo
 var pgClient = new pg.Client(pgConn);
 
 
-// Connect to Postgres Database
+/* Connect to Postgres Database */
 pgClient.connect(function(err) {
     if(err) {
         console.log('Could not connect to postgres', err); 
@@ -15,35 +15,66 @@ pgClient.connect(function(err) {
     }
 });
 
-function getUniqueItems(res, results, callback) {
-    var items = [];
-    async.eachSeries(results, function(area, cb){
+/**
+ * Function For Getting Unique Items in an Area
+ * @function
+ * @param {object} res - Response Object from ExpressJS
+ * @param {object} areas - Areas object for getting Items
+ * @param {function} callback - Callback Function
+ */
+function getUniqueItems(res, areas, uniqueItemsCallback) {
+    var areasWithItems = [];
+    async.eachSeries(areas, function(area, areas_callback){
         var pgAreaItemsQuery = "SELECT i.* FROM areas a LEFT JOIN unique_items i ON a.id = i.area_id WHERE a.id='" + area.id + "'";
-        var unique_items = pgClient.query(pgAreaItemsQuery, function(err, result){
+        var unique_items = pgClient.query(pgAreaItemsQuery, function(err, itemsForArea){
             if(err) {
                 res.send({error: 1, errorMsg: 'Error Receiving Items based on Area'})
                 return console.error('Error Receiving Items based on Area', err);
             } else {
-                var item = result.rows;
-                getTickets(res, item, function(response) {
+                var item = itemsForArea.rows;
+                //getTickets(res, item, function(itemsWithTickets) {
                     area.items = item;
-                    items.push(area);
-                });
+                    areasWithItems.push(area);
+                    if(areasWithItems.length < areas.length) {
+                        areas_callback();
+                    }
+                //});
             }
         });
         unique_items.on('end', function(results){
-            cb();
+            if(areas.length === areasWithItems.length) {
+                areas_callback();
+            }
         });
     }, function(){
-        if(typeof callback === "function") {
-            callback(items);
+        if(typeof uniqueItemsCallback === "function" && areas.length === areasWithItems.length) {
+            uniqueItemsCallback(areasWithItems);
         }
     });
 }
-function getTickets(res, results, callback) {
+
+/**
+ * Function that takes in items as object and returns items with any tickets
+ * they have
+ * @function
+ * @param {object} res - ExpressJS Res Object
+ * @param {object} items - Array of Item Objects
+ * @param {function} ticketsCallback - Callback Function
+ */
+function getTickets(res, items, ticketsCallback) {
+    // Instantiate Array of ticket objects to be returned
     var tickets = [];
-    async.eachSeries(results, function(item, cb) {
+
+    /*
+     * Iterate through each item
+     * Query PG for resulting tickets
+     * Append tickets to Item
+     * Return Item when done
+     */
+    async.eachSeries(items, function(item, item_cb) {
+        // Create PG Query Statement
         var pgAreaItemsQuery = "SELECT t.* FROM unique_items i LEFT JOIN tickets t ON t.fuid = i.id WHERE i.id='" + item.id + "'";
+        // Query PG for tickets
         var getTickets = pgClient.query(pgAreaItemsQuery, function(err, result){
             if(err) {
                 res.send({error: 1, errorMsg: 'Error Receiving Items based on Area'})
@@ -68,18 +99,18 @@ function getTickets(res, results, callback) {
 
 exports.findAll = function(req, res) {
     var pgQueryFindAll = "SELECT * FROM areas";
-    pgClient.query(pgQueryFindAll, function(err, result) {
+    pgClient.query(pgQueryFindAll, function(err, areas) {
         if(err) {
             res.send({error: 1, errMsg: 'Error Querying Hacker Tracker ' + err})
             return console.error('Error Querying Hacker Tracker', err);
         }
 
-        var results = result.rows;
+        var allAreas = areas.rows;
         var callback = req.query.callback
         var format = req.query.format;
-        getUniqueItems(res, results, function(response) {
+        getUniqueItems(res, allAreas, function(fullAreaInfo) {
             var areas_with_items = {};
-            areas_with_items.areas = response;
+            areas_with_items.areas = fullAreaInfo;
             if(typeof format != "undefined" || format != null) {
                 res.send(areas_with_items);
             } else {
@@ -91,19 +122,19 @@ exports.findAll = function(req, res) {
 exports.getById = function(req, res) {
     var id = req.route.params.id;
     var pgQueryFindByName = "SELECT * from areas WHERE id='"  + id + "'";
-    pgClient.query(pgQueryFindByName, function(err, result){
+    pgClient.query(pgQueryFindByName, function(err, areas){
         if(err) {
             res.send({error: 1, errMsg: 'Error Querying Hacker Tracker ' + err})
             return console.error('Error Querying Hacker Tracker', err);
         } else {
-            var results = result.rows;
+            var allAreas = areas.rows;
             var callback = req.query.callback;
             var format = req.query.format;
-            getUniqueItems(results, function(response) {
+            getUniqueItems(res, allAreas, function(fullAreaInfo) {
                 var areas_with_items = {};
-                areas_with_items.areas = response;
+                areas_with_items.areas = fullAreaInfo;
                 if(typeof format != "undefined" || format != null) {
-                    res.send(JSON.stringify(areas_with_items));
+                    res.send(areas_with_items);
                 } else {
                     res.send(callback + '(' + JSON.stringify(areas_with_items) + ')');
                 }
