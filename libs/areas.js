db5c29528229330c7c7de1e269a592d9f3fa2e6c
Danny Grove <danny@drgrovellc.com>
2013-11-07 23:52:15 -0500

    Modularizes querying and sending information to back to client to minimize duplicate code. Adds JSDoc3 documentation to functions

--- libs/areas.js
+++ libs/areas.js
@@ -4,6 +4,14 @@ var pg = require('pg'),
     uniqueItems = require('./unique_items'),
     redis = require('redis');
 
+// Create a Redis Client
+var redisClient = redis.createClient();
+
+// Display Redis Errors if any arise
+redisClient.on('error', function(err){
+    console.error('Redis Error: ', err)
+})
+
 // Create PG Connection String and Client
 var pgConn = "postgres://" + config.postgres.user + ":" + config.postgres.password + "@" + config.postgres.host + "/" + config.postgres.database;
 var pgClient = new pg.Client(pgConn);
@@ -17,57 +25,76 @@ pgClient.connect(function(err) {
     }
 });
 
-
-
 /**
- * Retreives all Areas, Items and Tickets
+ * Function for Querying Postgres and Retrieving Area Data
+ * @function queryPostgresForAreas
+ * @param {string} query - Query String for Postgres
+ * @param {object} pgClient - pgClient object
+ * @param {object} res - expressJS response object
+ * @callback {function} callback function for sending area(s) response
+ *
  */
-var findAll = function(req, res) {
-    var pgQueryFindAll = "SELECT * FROM areas";
-    pgClient.query(pgQueryFindAll, function(err, areas) {
+var queryPostgresForAreas = function(query, pgClient, res, callback) {
+    pgClient.query(query, function(err, areas) {
         if(err) {
             res.send({error: 1, errMsg: 'Error Querying Hacker Tracker ' + err})
             return console.error('Error Querying Hacker Tracker', err);
         }
 
         var allAreas = areas.rows;
-        var callback = req.query.callback
-        var format = req.query.format;
         uniqueItems.getUniqueItems(pgClient, res, allAreas, function(fullAreaInfo) {
             var areas_with_items = {};
             areas_with_items.areas = fullAreaInfo;
-            if(typeof format != "undefined" || format != null) {
-                res.send(areas_with_items);
-            } else {
-                res.send(callback + '(' + JSON.stringify(areas_with_items) + ')');
+            if(typeof callback === "function") {
+                callback(areas_with_items);
             }
         });
+    });
+}
+
+/**
+ * A function for taking sending JSON objects to the requesting client
+ * @function respondToClient
+ * @param {object} res - expressJS response object
+ * @param {object} options - options to decide type of response and jsonp
+ * callback
+ * @param {object} responseObject - Object being sent to client via res
+ */
+var respondToClient = function(res, options, responseObject){
+    if(typeof options.format != "undefined" || options.format != null) {
+        res.send(responseObject);
+    } else {
+        res.send(callback + '(' + JSON.stringify(responseObject) + ')');
+    }
+}
+
+/**
+ * Retreives all Areas, Items and Tickets
+ * @function findAll
+ * @param {object} req - expressJS request object
+ * @param {object} res - expressJS response object
+ */
+var findAll = function(req, res) {
+    var responseOptions = {};
+    responseOptions.callback = req.query.callback;
+    responseOptions.format = req.query.format;
+    var pgQueryFindAll = "SELECT * FROM areas";
+    queryPostgresForAreas(pgQueryFindAll, pgClient, res, function(allAreas){
+        respondToClient(res, responseOptions, allAreas);
     });
 }
 /**
  * Retreives Area, Items and Tickets by Id
  */
 var getById = function(req, res) {
+    var responseOptions = {};
+    responseOptions.callback = req.query.callback;
+    responseOptions.format = req.query.format;
+    
     var id = req.route.params.id;
-    var pgQueryFindByName = "SELECT * from areas WHERE id='"  + id + "'";
-    pgClient.query(pgQueryFindByName, function(err, areas){
-        if(err) {
-            res.send({error: 1, errMsg: 'Error Querying Hacker Tracker ' + err})
-            return console.error('Error Querying Hacker Tracker', err);
-        } else {
-            var allAreas = areas.rows;
-            var callback = req.query.callback;
-            var format = req.query.format;
-            uniqueItems.getUniqueItems(pgClient, res, allAreas, function(fullAreaInfo) {
-                var areas_with_items = {};
-                areas_with_items.areas = fullAreaInfo;
-                if(typeof format != "undefined" || format != null) {
-                    res.send(areas_with_items);
-                } else {
-                    res.send(callback + '(' + JSON.stringify(areas_with_items) + ')');
-                }
-            });
-        }
+    var pgQueryFindById = "SELECT * from areas WHERE id='"  + id + "'";
+    queryPostgresForAreas(pgQueryFindById, pgClient, res, function(area){
+        respondToClient(res, responseOptions, area);
     });
 }
 
