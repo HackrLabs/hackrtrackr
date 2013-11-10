ba3d3a59d789999627167dfb7d7b9dc934747a9a
Danny Grove <danny@drgrovellc.com>
2013-11-09 22:57:16 -0500

    Adds JSDoc3 Documentation

--- libs/response.js
+++ libs/response.js
@@ -1,5 +1,13 @@
 'use strict';
 
+/**
+ * Function for Creating JSON Responses
+ * @function createResponse
+ * @param {object || string} msg - An object or string that will be returned as
+ * response
+ * @param {boolean} err - Is it an error
+ * @return {object} response - Response JSON
+ */
 var createResponse = function(msg, err) {
     var response = {};
     response.error = err || false;
