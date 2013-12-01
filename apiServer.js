2fca8dd2d8eeedf613bb8dc99a208be211e13996
Danny Grove <danny@drgrovellc.com>
2013-12-01 16:17:59 -0500

    Moves labaccess to dooraccess.

--- apiServer.js
+++ apiServer.js
@@ -1,5 +1,5 @@
 var express = require('express'),
-    labaccess = require('./libs/labaccess'),
+    dooraccess = require('./libs/dooraccess'),
     areas = require('./libs/areas');
 
 var app = express();
@@ -9,8 +9,8 @@ app.get('/', function(req, res) {
     res.send('This page is not active');
 });
 
-app.get('/labaccess', labaccess.findAll);
-app.get('/labaccess/:id', labaccess.getByMemberId);
+app.get('/dooraccess', dooraccess.getAll);
+app.get('/dooraccess/:id', dooraccess.getByMemberId);
 app.get('/areas', areas.findAll);
 app.get('/areas/:id', areas.getById);
 
