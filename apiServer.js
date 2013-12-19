0740df1be411d67a57766605b465bdc40da468c8
Danny Grove <danny@drgrovellc.com>
2013-12-19 17:00:52 -0500

    Adds Member Info

--- apiServer.js
+++ apiServer.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     dooraccess = require('./libs/dooraccess'),
-    areas = require('./libs/areas');
+    areas = require('./libs/areas'),
+    members = require('./libs/members');
 
 var app = express();
 app.set('domain', 'api.hackertracker.dev');
@@ -13,6 +14,8 @@ app.get('/dooraccess', dooraccess.getAll);
 app.get('/dooraccess/:id', dooraccess.getByMemberId);
 app.get('/areas', areas.findAll);
 app.get('/areas/:id', areas.getById);
+app.get('/members', members.getAll);
+app.get('/members/:id', members.getById);
 
 app.listen(1234);
 console.log('Listening on port 1234');
