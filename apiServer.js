6aba78a136c07ecc9d8e2006187aa5bf3e9b82a4
Danny Grove <danny@drgrovellc.com>
2013-12-23 15:37:50 -0500

    Adds CORS code

--- apiServer.js
+++ apiServer.js
@@ -6,9 +6,22 @@ var areas = require('./libs/areas'),
     config = require('./libs/config');
 
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    } else {
+        next();
+    }
+};
 var app = express();
 app.set('domain', config.app.domain);
 app.use(express.bodyParser());
+app.use(allowCrossDomain);
 app.namespace(config.app.namespace, function(){
     app.get('/', function(req, res) {
         res.send('This page is not active');
