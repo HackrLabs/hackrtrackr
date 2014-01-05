c6a0fe701d5cb53c69c75035386814f362b56f5d
Danny Grove <danny@drgrovellc.com>
2014-01-05 09:18:53 -0500

    Changes subdomain to merchant

--- api/libs/database/sql/api/account.sql
+++ api/libs/database/sql/api/account.sql
@@ -1,6 +1,6 @@
-CREATE TABLE accounts (
+CnREATE TABLE accounts (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   accountName varchar(100) not null,
-  subdomin varchar(100) not null,
+  merchant varchar(100) not null,
   isActive boolean default true
 );
